35d712161ad5fcd519136704502aa481d3f85ba3
andrewnicols <andrewnicols@users.noreply.github.com>
2022-08-14 03:52:07 +0000

    deploy: 980ec78b700b717b0fcd7c6863a7865f4da68f08

--- 3.11/search_config.php
+++ 3.11/search_config.php
@@ -16,7 +16,7 @@ $translator = array(
   'search_matches' => "Matches:",
   'search' => "Search",
   'split_bar' => "",
-  'logo' => "Generated on Sun Aug 7 2022 03:23:16 for Moodle PHP Documentation by&#160;\n<a href=\"https://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.svg\" width=\"104\" height=\"31\" alt=\"doxygen\"/></a> 1.9.4 ",
+  'logo' => "Generated on Sun Aug 14 2022 03:33:10 for Moodle PHP Documentation by&#160;\n<a href=\"https://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.svg\" width=\"104\" height=\"31\" alt=\"doxygen\"/></a> 1.9.4 ",
 );
 
 ?>
--- master/search_config.php
+++ master/search_config.php
@@ -16,7 +16,7 @@ $translator = array(
   'search_matches' => "Matches:",
   'search' => "Search",
   'split_bar' => "",
-  'logo' => "Generated on Sun Aug 7 2022 03:30:58 for Moodle PHP Documentation by&#160;\n<a href=\"https://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.svg\" width=\"104\" height=\"31\" alt=\"doxygen\"/></a> 1.9.4 ",
+  'logo' => "Generated on Sun Aug 14 2022 03:44:37 for Moodle PHP Documentation by&#160;\n<a href=\"https://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.svg\" width=\"104\" height=\"31\" alt=\"doxygen\"/></a> 1.9.4 ",
 );
 
 ?>
