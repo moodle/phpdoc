31ff092f3e6dad498c5f5a194bebcefbf7bc29bb
andrewnicols <andrewnicols@users.noreply.github.com>
2022-10-16 04:39:36 +0000

    deploy: 980ec78b700b717b0fcd7c6863a7865f4da68f08

--- 3.9/search_config.php
+++ 3.9/search_config.php
@@ -16,7 +16,7 @@ $translator = array(
   'search_matches' => "Matches:",
   'search' => "Search",
   'split_bar' => "",
-  'logo' => "Generated on Sun Oct 9 2022 04:02:09 for Moodle PHP Documentation by&#160;\n<a href=\"https://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.svg\" width=\"104\" height=\"31\" alt=\"doxygen\"/></a> 1.9.5 ",
+  'logo' => "Generated on Sun Oct 16 2022 04:11:25 for Moodle PHP Documentation by&#160;\n<a href=\"https://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.svg\" width=\"104\" height=\"31\" alt=\"doxygen\"/></a> 1.9.5 ",
 );
 
 ?>
