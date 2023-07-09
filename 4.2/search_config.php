ea2298a875f875f118792adf535c816274018535
andrewnicols <andrewnicols@users.noreply.github.com>
2023-07-09 03:54:30 +0000

    deploy: 53d4943659b53c23a4dafa3f104248e05236bd20

--- 4.2/search_config.php
+++ 4.2/search_config.php
@@ -16,7 +16,7 @@ $translator = array(
   'search_matches' => "Matches:",
   'search' => "Search",
   'split_bar' => "",
-  'logo' => "Generated on Sun Jul 2 2023 03:34:52 for Moodle PHP Documentation by&#160;\n<a href=\"https://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.svg\" width=\"104\" height=\"31\" alt=\"doxygen\"/></a> 1.9.7 ",
+  'logo' => "Generated on Sun Jul 9 2023 03:37:39 for Moodle PHP Documentation by&#160;\n<a href=\"https://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.svg\" width=\"104\" height=\"31\" alt=\"doxygen\"/></a> 1.9.7 ",
 );
 
 ?>
