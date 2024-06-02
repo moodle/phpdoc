86a3bf50be6ddc942e317a664f043d7002da8275
andrewnicols <andrewnicols@users.noreply.github.com>
2024-06-02 02:59:50 +0000

    deploy: 8e5f75cf2b1661a9b4118b381bfac9bb90b1d33f

--- main/search_config.php
+++ main/search_config.php
@@ -16,7 +16,7 @@ $translator = array(
   'search_matches' => "Matches:",
   'search' => "Search",
   'split_bar' => "<script type=\"text/javascript\">\n/* @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&amp;dn=expat.txt MIT */\n$(function(){ initResizable(false); });\n/* @license-end */\n</script>\n",
-  'logo' => "Generated on Sun May 26 2024 02:52:20 for Moodle PHP Documentation by&#160;\n<a href=\"https://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.svg\" width=\"104\" height=\"31\" alt=\"doxygen\"/></a> 1.11.0 ",
+  'logo' => "Generated on Sun Jun 2 2024 02:54:09 for Moodle PHP Documentation by&#160;\n<a href=\"https://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.svg\" width=\"104\" height=\"31\" alt=\"doxygen\"/></a> 1.11.0 ",
 );
 
 ?>
