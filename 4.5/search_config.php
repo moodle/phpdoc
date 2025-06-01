94da697195ffaedebf91654209453c0d5538c102
junpataleta <junpataleta@users.noreply.github.com>
2025-06-01 04:11:13 +0000

    deploy: e2dec3f747fe1188cbc0c03b231d9ec7a57f4c8f

---
+++ 4.5/search_config.php
@@ -0,0 +1,22 @@
+<?php
+
+$config = array(
+  'PROJECT_NAME' => "Moodle PHP Documentation",
+  'GENERATE_TREEVIEW' => false,
+  'DISABLE_INDEX' => false,
+);
+
+$translator = array(
+  'search_results_title' => "Search Results",
+  'search_results' => array(
+    0 => "Sorry, no documents matching your query.",
+    1 => "Found <b>1</b> document matching your query.",
+    2 => "Found <b>\$num</b> documents matching your query. Showing best matches first.",
+  ),
+  'search_matches' => "Matches:",
+  'search' => "Search",
+  'split_bar' => "<script type=\"text/javascript\">\n/* @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&amp;dn=expat.txt MIT */\n$(function(){ initResizable(false); });\n/* @license-end */\n</script>\n",
+  'logo' => "Generated on Sun Jun 1 2025 03:51:15 for Moodle PHP Documentation by&#160;\n<a href=\"https://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.svg\" width=\"104\" height=\"31\" alt=\"doxygen\"/></a> 1.13.2 ",
+);
+
+?>
